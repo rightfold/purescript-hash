5758d94231a26065b48bbcc50c6a76780595fa98
rightfold <rightfold@gmail.com>
2016-04-26 21:04:16 +0200

    Add instance for Unit

--- src/Data/Hash.purs
+++ src/Data/Hash.purs
@@ -13,16 +13,19 @@ import Prelude
 class Hash a where
   hash :: a -> Int
 
+instance hashUnit :: Hash Unit where
+  hash _ = 1
+
+instance hashBoolean :: Hash Boolean where
+  hash true = 1
+  hash false = 1073741823
+
 instance hashInt :: Hash Int where
   hash = id
 
 instance hashNumber :: Hash Number where
   hash = numberToString >>> hash
 foreign import numberToString :: Number -> String
-
-instance hashBoolean :: Hash Boolean where
-  hash true = 1
-  hash false = 1073741823
 
 instance hashFoldable :: (Foldable f, Hash a) => Hash (f a) where
   hash = foldl (\h x -> combine h (hash x)) 1
