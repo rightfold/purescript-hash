afab6c515a3c9e9469baf084138b6dd87454981a
rightfold <rightfold@gmail.com>
2016-04-26 21:29:39 +0200

    Implement generic hash function
    Fixes #1.

--- src/Data/Hash.purs
+++ src/Data/Hash.purs
@@ -7,34 +7,41 @@ module Data.Hash
 
 import Data.Char as Char
 import Data.Foldable (class Foldable, foldl)
+import Data.Generic (class Generic, GenericSpine(..), toSpine)
 import Data.String as String
 import Prelude
 
 class Hash a where
   hash :: a -> Int
 
+instance hashGenericSpine :: Hash GenericSpine where
+  hash (SProd name fields) = combine (hash name) (hash $ fields # map (_ $ unit))
+  hash (SRecord fields) = combine (hash keys) (hash vals)
+    where keys = fields # map _.recLabel
+          vals = fields # map (_.recValue >>> (_ $ unit))
+  hash (SNumber v)  = hash $ numberToString v
+  hash (SBoolean v) = if v then 1 else 1073741823
+  hash (SInt v)     = v
+  hash (SString s)  = hash $ String.toCharArray s
+  hash (SChar c)    = Char.toCharCode c
+  hash (SArray xs)  = hash $ xs # map (_ $ unit)
+
 instance hashUnit :: Hash Unit where
   hash _ = 1
 
-instance hashBoolean :: Hash Boolean where
-  hash true = 1
-  hash false = 1073741823
-
-instance hashInt :: Hash Int where
-  hash = id
-
-instance hashNumber :: Hash Number where
-  hash = numberToString >>> hash
-foreign import numberToString :: Number -> String
+instance hashNumber  :: Hash Number  where hash = gHash
+instance hashBoolean :: Hash Boolean where hash = gHash
+instance hashInt     :: Hash Int     where hash = gHash
+instance hashString  :: Hash String  where hash = gHash
+instance hashChar    :: Hash Char    where hash = gHash
 
 instance hashFoldable :: (Foldable f, Hash a) => Hash (f a) where
   hash = foldl (\h x -> combine h (hash x)) 1
 
-instance hashChar :: Hash Char where
-  hash = Char.toCharCode
-
-instance hashString :: Hash String where
-  hash = String.toCharArray >>> hash
-
 combine :: Int -> Int -> Int
 combine x y = x + 31 * x + y
+
+gHash :: forall a. (Generic a) => a -> Int
+gHash = toSpine >>> hash
+
+foreign import numberToString :: Number -> String
